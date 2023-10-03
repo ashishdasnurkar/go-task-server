0a556419e0913ba207eba10ada7c39b844db0a15
Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
2023-10-03 17:45:03 +1100

    WIP: taskserver sync impl
    Co-authored-by: Ashish Dasnurkar <ashish.dasnurkar@gmail.com>
    Co-authored-by: Thameera Senanayaka <me@thameera.com>

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/ashishdasnurkar/go-task-server
 
 go 1.21.0
 
-require github.com/gorilla/mux v1.8.0 // indirect
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
+	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/mux"
 )
 
@@ -17,7 +22,23 @@ func main() {
 }
 
 func syncTasks(w http.ResponseWriter, r *http.Request) {
-	
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal("Reading body error: ", err)
+	}
+	fmt.Println(string(reqBody))
+	taskArr, err := base64.StdEncoding.DecodeString(string(reqBody))
+	if err != nil {
+		log.Fatal("Reading body error: ", err)
+	}
+
+	var taskObj TaskType
+	err = proto.Unmarshal(taskArr, &taskObj)
+	if err != nil {
+		log.Fatal("Unmarshaling error: ", err)
+	}
+
+	fmt.Println(taskObj)
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
